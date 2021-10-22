3ad10737252aebc07913392169225da175281cd8
Daniel Sitarek <deltasierra777@gmail.com>
2021-10-21 22:08:42 -0500

    integrating nhl api saving work

--- src/components/CreatePlayer.js
+++ src/components/CreatePlayer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPlayer, updatePlayer } from '../data/playerData';
+import { getTeams, getRoster } from '../data/nhlData';
 
 const initialState = {
   name: '',
@@ -12,18 +13,32 @@ export default function CreatePlayer({
   player, setPlayers, uid, setEditItem,
 }) {
   const [formInput, setFormInput] = useState(initialState);
+  const [teams, setTeams] = useState([]);
+  const [playerInput, setPlayerInput] = useState('');
+
+  useEffect(() => {
+    getTeams().then((data) => setTeams(data));
+  }, []);
 
   useEffect(() => {
     if (player.firebaseKey) {
       setFormInput({
-        name: player.name,
+        name: `${player.name}-${player.personId}`,
         firebaseKey: player.firebaseKey,
         position: player.position,
         imageURL: player.imageURL,
         uid: player.uid,
+        team: player.team,
       });
     }
   }, [player]);
+
+  useEffect(async () => {
+    if (formInput.team) {
+      const people = await getRoster(formInput.team);
+      setPlayerInput(people);
+    }
+  }, [formInput]);
 
   const handleChange = (e) => {
     setFormInput((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
@@ -35,14 +50,25 @@ export default function CreatePlayer({
   };
 
   const handleSubmit = (e) => {
+    const objFbKey = (player.firebaseKey || '');
+    const personId = (formInput.name).split('-')[1];
+    const fbObj = {
+      name: (formInput.name).split('-')[0],
+      firebaseKey: objFbKey,
+      position: formInput.position,
+      imageURL: `https://nhl.bamcontent.com/images/headshots/current/168x168/${personId}.jpg`,
+      uid,
+      personId,
+      team: formInput.team,
+    };
     e.preventDefault();
     if (player.firebaseKey) {
-      updatePlayer(player.firebaseKey, formInput).then((players) => {
+      updatePlayer(player.firebaseKey, fbObj).then((players) => {
         setPlayers(players);
         resetForm();
       });
     } else {
-      createPlayer({ ...formInput, uid }).then((players) => {
+      createPlayer(fbObj).then((players) => {
         setPlayers(players);
         resetForm();
       });
@@ -52,11 +78,17 @@ export default function CreatePlayer({
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit} className="player-form">
-        <label>Player Name:
-          <input type="text" id="nameInput" name="name" value={formInput.name} onChange={handleChange} required />
+        <label>Team:
+          <select className="form-select" aria-label="Default select example" id="team" name="team" value={formInput.team} onChange={handleChange} required>
+            <option value="">Select Team</option>
+            {teams.map((team) => <option key={`team-${team.id}`} value={team.id || ''}>{team.name}</option>)}
+          </select>
         </label>
-        <label>Image URL:
-          <input type="text" id="ImageURL" name="imageURL" value={formInput.imageURL} onChange={handleChange} required />
+        <label>Player Name:
+          <select className="form-select" aria-label="Default select example" id="name" name="name" value={formInput.name} onChange={handleChange} disabled={!formInput.team} required>
+            <option value="">Select Player</option>
+            {playerInput ? playerInput.map((person) => <option key={`player-${person.id}`} value={`${person.name}-${person.id}` || ''}>{person.name}</option>) : ''}
+          </select>
         </label>
         <label>Position:
           <select className="form-select" aria-label="Default select example" id="position" name="position" value={formInput.position || ''} onChange={handleChange} required>
@@ -83,6 +115,8 @@ CreatePlayer.propTypes = {
     position: PropTypes.string,
     imageURL: PropTypes.string,
     uid: PropTypes.string,
+    team: PropTypes.string,
+    personId: PropTypes.string,
   }),
   setPlayers: PropTypes.func.isRequired,
   uid: PropTypes.string.isRequired,
